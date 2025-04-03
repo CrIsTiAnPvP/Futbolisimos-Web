4b10a6818e2d5df96ff0fd87186168e57e37c04e
CrIsTiiAnPvP <65726181+CrIsTiAnPvP@users.noreply.github.com.>
2025-04-04 01:02:13 +0200

    Auth commit: - Added oauth apps to login (GitHub, Google & Spotify) - Implemented Session interface to specify Session props - Added icons - Added i18n & session support to main app in layout.tsx

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
+import SpotifyProvider from "next-auth/providers/spotify";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
+import { prisma } from "@/prisma";
+
+export default NextAuth({
+	providers: [
+		Github({
+			clientId: process.env.GITHUB_ID || "",
+			clientSecret: process.env.GITHUB_SECRET || "",
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_ID || "",
+			clientSecret: process.env.GOOGLE_SECRET || "",
+		}),
+		SpotifyProvider({
+			clientId: process.env.SPOTIFY_ID || "",
+			clientSecret: process.env.SPOTIFY_SECRET || "",
+		})
+	],
+	adapter: PrismaAdapter(prisma)
+});
